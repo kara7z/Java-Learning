ccbe2d3f514ee6b3df9da719a0215952fb8c0581
kara7z <kara.ossama12@gmail.com>
2026-09-03 15:52:26 +0100

    Add Factoriel.java

---
+++ learning/Factoriel.java
@@ -0,0 +1,16 @@
+
+class Factoriel {
+
+  int factoriel(int number) {
+    int result = 1;
+    if (number > 1) {
+      for (int i = 1; i <= number; i++) {
+        result *= i;
+      }
+    }
+    else {
+      result = -1;
+    }
+    return result;
+  }
+}
