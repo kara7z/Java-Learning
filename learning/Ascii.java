c76b02ca59927066ce656c26a4490ccd4b2ff7d1
kara7z <kara.ossama12@gmail.com>
2026-09-03 15:52:26 +0100

    Add Ascii.java

---
+++ learning/Ascii.java
@@ -0,0 +1,15 @@
+class Ascii {
+  char character;
+
+  Ascii(char c) {
+    character = c;
+
+  }
+
+  int Decimal() {
+    char charact = character;
+    int result = charact;
+    return result;
+  }
+
+}
