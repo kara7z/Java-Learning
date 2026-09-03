2b255b0605bd0bbab0c5ae983c76ef9567db2ae8
kara7z <kara.ossama12@gmail.com>
2026-09-03 15:52:26 +0100

    Add Recurcive.java

---
+++ learning/Recurcive.java
@@ -0,0 +1,17 @@
+
+class Recurcive {
+  boolean exec(String word) {
+    boolean isRecursive = true;
+    char c1, c2;
+
+    for (int i = 0, j = word.length() - 1; i <= j; i++, j--) {
+      c1 = word.charAt(i);
+      c2 = word.charAt(j);
+
+      if (c1 != c2) {
+        isRecursive = false;
+      }
+    }
+    return isRecursive;
+  }
+}
